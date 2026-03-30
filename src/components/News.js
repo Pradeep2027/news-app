696ac1cf0db436a7ca821994a8ca2d432a3b53af
Pradeep2027 <shabadpradeep1@gmail.com>
2026-03-31 01:46:42 +0530

    Displayed news from json file

--- src/components/News.js
+++ src/components/News.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem';
+import output from '../sampleOutput.json'
 
 export class News extends Component {
+  constructor(){
+    super();
+    this.state = {
+        articles: output.articles
+    }
+  }
   render() {
     return (
       <>
       <h2 className="mx-5 my-3">News Monkey - Top Headlines</h2>
         <div className="container my-3">
           <div className="row my-2">
-            <NewsItem title="News Title1" description="description1"></NewsItem>
-            <NewsItem title="News Title2" description="description2"></NewsItem>
-            <NewsItem title="News Title3" description="description3"></NewsItem>
-          </div>
-          <div className="row my-2">
-            <NewsItem title="News Title4" description="description4"></NewsItem>
-            <NewsItem title="News Title5" description="description5"></NewsItem>
-            <NewsItem title="News Title6" description="description6"></NewsItem>
+            {this.state.articles.filter(a=>a.title && a.description).map((a)=>{
+                return <NewsItem key={a.url} title={a.title.slice(0,47)+'...'} description={a.description.slice(0,97)+'...'} urlToImage={a.urlToImage} url={a.url}></NewsItem>
+            })}
           </div>
         </div>
       </>
