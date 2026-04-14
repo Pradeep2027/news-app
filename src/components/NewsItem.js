e3792ab8179ff72d3ac87481b820c79c9372b247
Pradeep2027 <shabadpradeep1@gmail.com>
2026-04-15 00:23:28 +0530

    Converted NewsItem from Class based to function based component

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import dummyImage from './../news.jpg'
-export class NewsItem extends Component {
-  render() {
-    let {title, description, urlToImage, url, author, date, source} = this.props;
-    return (
-      <div className="col-md-4 col-sm-6 mb-2">
-        <div className="card">
-          <div className="d-flex justify-content-end position-absolute right-0">
-            <span className="badge rounded-pill bg-info">{source}</span>
-          </div>
-          <img src={urlToImage?urlToImage:dummyImage} className="card-img-top" alt="image-not-found" />
-          <div className="card-body">
-            <h5 className="card-title">{title ?title.slice(0,47)+'...' : ''}</h5>
-            <p className="card-text">{description ? description.slice(0,97)+'...' : ''}</p>
-            <p className="card-text"><small className="text-muted">By {author?author:'Unknown'} on {new Date(date).toLocaleDateString()}</small></p>
-            <a href={url} target='_blank' rel="noreferrer" className="btn btn-sm btn-primary">More Info.</a>
-          </div>
+
+export default function NewsItem(props) {
+  let {title, description, urlToImage, url, author, date, source} = props;
+  return (
+    <div className="col-md-4 col-sm-6 mb-2">
+      <div className="card">
+        <div className="d-flex justify-content-end position-absolute right-0">
+          <span className="badge rounded-pill bg-info">{source}</span>
+        </div>
+        <img src={urlToImage?urlToImage:dummyImage} className="card-img-top" alt="image-not-found" />
+        <div className="card-body">
+          <h5 className="card-title">{title ?title.slice(0,47)+'...' : ''}</h5>
+          <p className="card-text">{description ? description.slice(0,97)+'...' : ''}</p>
+          <p className="card-text"><small className="text-muted">By {author?author:'Unknown'} on {new Date(date).toLocaleDateString()}</small></p>
+          <a href={url} target='_blank' rel="noreferrer" className="btn btn-sm btn-primary">More Info.</a>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default NewsItem;
